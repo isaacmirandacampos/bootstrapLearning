45cbb9f82b8a636ce31500b64564ebd1e217270a
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-09-14 14:08:44 -0300

    finalizado projetinho do bootstrap

---
+++ src/navbar-animation-fix.js
@@ -0,0 +1,2 @@
+$('#collapse-navbar').on('show.bs.collapse', ()=> $('.topCasaFina-banner').css('transform','translate(-50%,10%)'));
+$('#collapse-navbar').on('hide.bs.collapse', ()=> $('.topCasaFina-banner').css('transform','translate(-50%,-50%)'));
